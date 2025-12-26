a496cae3fec7bce9ea2d9a04d6e37fa121caf2cc
Sandeep_balaveni <100793974+sundeep-balaveni@users.noreply.github.com>
2025-12-26 15:31:45 +0530

    Update metrics.js

--- metrics.js
+++ metrics.js
@@ -1,18 +1,9 @@
-function random(min, max) {
+function rand(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 setInterval(() => {
-  cpuData.shift();
-  cpuData.push(random(20, 90));
-
-  memoryData.shift();
-  memoryData.push(random(30, 85));
-
-  networkData.shift();
-  networkData.push(random(10, 70));
-
-  cpuChart.update();
-  memoryChart.update();
-  networkChart.update();
+  document.getElementById("cpuVal").innerText = rand(20, 85) + "%";
+  document.getElementById("memVal").innerText = rand(40, 90) + "%";
+  document.getElementById("netVal").innerText = rand(5, 50) + " Mbps";
 }, 2000);
